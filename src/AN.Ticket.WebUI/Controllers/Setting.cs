28c3b64e9ac86e1f60f055e198ba884849b5868a
Guilherme Almeida <guidevtec@gmail.com>
2024-09-27 00:17:48 -0300

    Criação da tela de configuração

---
+++ src/AN.Ticket.WebUI/Controllers/Setting.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AN.Ticket.WebUI.Controllers
+{
+    public class Setting : Controller
+    {
+        private readonly ILogger<Setting> _logger;
+
+        public Setting(ILogger<Setting> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
