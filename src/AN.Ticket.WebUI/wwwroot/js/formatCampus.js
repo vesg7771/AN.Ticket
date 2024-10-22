2b44a3f33e70f3563eb009e8ff35cab30c603d85
Guilherme Almeida <guidevtec@gmail.com>
2024-10-22 09:30:17 -0300

    ajuste para diferenciar numero de celular

--- src/AN.Ticket.WebUI/wwwroot/js/formatCampus.js
+++ src/AN.Ticket.WebUI/wwwroot/js/formatCampus.js
@@ -18,7 +18,7 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js', 
         // Aplicar máscaras após carregar todas as bibliotecas
         $(document).ready(function() {
             $('#Cpf').mask('000.000.000-00');
-            $('#Phone').mask('(00) 0000-0000');
+            $('#Phone').mask('(00) 00000-0000');
             $('#Mobile').mask('(00) 00000-0000');
         });
     });
